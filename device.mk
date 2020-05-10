f66678d1b4dc1cf1f8d409d341c245751e1d92b2
alexeei <megaxaser@gmail.com>
2020-05-10 15:00:44 +0300

    11.5.334

--- device.mk
+++ device.mk
@@ -159,6 +159,10 @@ PRODUCT_PACKAGES += \
     ethertypes \
     libebtc
 
+# IRSC
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
+
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
