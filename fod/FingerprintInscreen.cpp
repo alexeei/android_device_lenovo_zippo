50742a5f0faaf02d2bcdfb6ede6ea5bc46bee276
alexeei <alexeei@yandex.ru>
2020-05-20 23:50:37 -0700

    fix fod

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -120,8 +120,8 @@ Return<void> FingerprintInscreen::setLongPressEnabled(bool) {
 }
 
 Return<int32_t> FingerprintInscreen::getDimAmount(int32_t brightness) {
- ./*  return(int32_t)(255 * (1.0 - pow(brightness / 1023.0f, 0.455)));
-	  */
+ /*  return(int32_t)(255 * (1.0 - pow(brightness / 1023.0f, 0.455))); */
+	  
  return 255 - brightness;
 	
 	/*
