c069144710a9c1365698a493caf6df912554846f
alexeei <alexeei@yandex.ru>
2020-05-24 05:33:39 -0700

    delete twrp

--- system_prop.mk
+++ system_prop.mk
@@ -147,8 +147,8 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp,adb \
     ro.adb.secure=0 \
-    ro.secure=0 \
-    ro.debuggable=1    
+    ro.secure=0 
+   
 
 # WiFi Display
 PRODUCT_PRODUCT_PROPERTIES += \
