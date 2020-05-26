a353c9e37f393037ec85f4a8a64f8787045013f6
alexeei <alexeei@yandex.ru>
2020-05-26 09:40:01 +0300

    Revert "delete nocutout"
    This reverts commit b4a9e5e62681065a664ee792fcc0e5c529c003e2.

--- device.mk
+++ device.mk
@@ -182,6 +182,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/slip-cover.kl:system/usr/keylayout/slip-cover.kl
 
 
+# NoCutout
+PRODUCT_PACKAGES += \
+    NoCutoutOverlay
 
 # Face Unlock
 TARGET_FACE_UNLOCK_SUPPORTED := false
