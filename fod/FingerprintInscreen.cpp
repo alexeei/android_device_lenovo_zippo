cd4505c97aeba634c3a28df0dd55484278e1b2e5
alexeei <alexeei@yandex.ru>
2020-06-04 14:54:56 +0300

    Revert "fod: switch to in-kernel handling of changes"
    This reverts commit f302cbddbd92f6e9d535d663e77ac037321ab17f.

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -37,6 +37,15 @@
 
 #define CMD_FINGERPRINT_EVENT 10
 
+#define FOD_HBM_PATH "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/fod_hbm"
+#define FOD_HBM_ON 1
+#define FOD_HBM_OFF 0
+
+#define DC_STATUS_PATH "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/msm_fb_ea_enable"
+#define DC_STATUS_ON 1
+#define DC_STATUS_OFF 0
+
+#define BRIGHTNESS_PATH "/sys/class/backlight/panel0-backlight/brightness"
 
 namespace vendor {
 namespace lineage {
@@ -58,6 +67,14 @@ static void set(const std::string& path, const T& value) {
     
 }
 
+template <typename T>
+static T get(const std::string& path, const T& def) {
+    std::ifstream file(path);
+    T result;
+
+    file >> result;
+    return file.fail() ? def : result;
+}
 
 
 
@@ -76,6 +93,7 @@ Return<void> FingerprintInscreen::onFinishEnroll() {
 }
 
 Return<void> FingerprintInscreen::onPress() {
+    set(FOD_HBM_PATH, FOD_HBM_ON);
     this->mVendorFpService->goodixExtendCommand(CMD_FINGERPRINT_EVENT, 1);
     
 
@@ -83,6 +101,7 @@ Return<void> FingerprintInscreen::onPress() {
 }
 
 Return<void> FingerprintInscreen::onRelease() {
+    set(FOD_HBM_PATH, FOD_HBM_OFF);
     this->mVendorFpService->goodixExtendCommand(CMD_FINGERPRINT_EVENT, 0);
   
 
@@ -139,8 +158,12 @@ Return<void> FingerprintInscreen::setLongPressEnabled(bool) {
 }
 
 Return<int32_t> FingerprintInscreen::getDimAmount(int32_t)  {
+    int realBrightness = get(BRIGHTNESS_PATH, 0);
     
-    return 0;
+
+    double dim = (255 + ((-12.08071) * pow((double)realBrightness, 0.4)));
+    
+    return (int32_t)dim;
     
     
 
