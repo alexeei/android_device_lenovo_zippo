c537a7b7c3a1296dfb1a66174fe58372dcb3aeb9
alexeei <alexeei@yandex.ru>
2020-07-08 23:33:18 +0300

    fixes

--- system_prop.mk
+++ system_prop.mk
@@ -7,7 +7,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.btstack.connect.peer_earbud=true  \
     persist.vendor.btstack.enable.splita2dp=true \
     persist.bt.enableAptXHD=true \
-    persist.service.btui.use_aptx=1 \
     vendor.qcom.bluetooth.soc=cherokee
 
 
