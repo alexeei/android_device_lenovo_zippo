c27cbb8f603a838f6d8765f9782e542b8f7a5d3b
alexeei <alexeei@yandex.ru>
2020-07-08 23:53:06 +0300

    bluetooth qti

--- device.mk
+++ device.mk
@@ -40,6 +40,7 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Permissions
 PRODUCT_COPY_FILES += \
+    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:system/etc/permissions/android.hardware.audio.pro.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
@@ -104,9 +105,26 @@ PRODUCT_COPY_FILES += \
 
 
 #Aptx
+#PRODUCT_COPY_FILES += \
+  #  $(LOCAL_PATH)/bluetooth/libaptX_encoder.sosystem/lib64/libaptX_encoder.so 
+   #  $(LOCAL_PATH)/bluetooth/libaptXHD_encoder.sosystem/lib64/libaptXHD_encoder.so
+     
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth.audio@2.0-impl \
+    audio.bluetooth.default \
+    BluetoothQti \
+    libbthost_if \
+    libldacBT_dec \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor
+    
+    PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
+
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/bluetooth/libaptX_encoder.so:system/lib64/libaptX_encoder.so \
-     $(LOCAL_PATH)/bluetooth/libaptXHD_encoder.so:system/lib64/libaptXHD_encoder.so
+    hardware/qcom-caf/sm8150/audio/configs/common/bluetooth_qti_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_qti_audio_policy_configuration.xml
 
 # Camera
 PRODUCT_PACKAGES += \
