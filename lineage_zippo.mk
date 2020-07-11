64f8e8a9a4bb94ccd48f0bf526ea9d5bf482e673
alexeei <megaxaser@gmail.com>
2020-07-11 18:17:20 +0300

    Revert "try fix fm"
    This reverts commit 759e3b64d608684c90c25b05f95b5847fef08a6d.

--- lineage_zippo.mk
+++ lineage_zippo.mk
@@ -7,7 +7,7 @@
 $(call inherit-product, device/lenovo/zippo/device.mk)
 
 # Inherit some common stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_mini_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
--- system_prop.mk
+++ system_prop.mk
@@ -170,5 +170,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
 persist.vendor.memplus.enable=1
 
 # Zygote preforking
-PRODUCT_PROPERTY_OVERRIDES += \
 persist.device_config.runtime_native.usap_pool_enabled=true
