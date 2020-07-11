3397f5f532ce1d9dab53e94775889583c9bb9c5d
alexeei <megaxaser@gmail.com>
2020-07-11 18:14:06 +0300

    Revert "try fix bt"
    This reverts commit ed8a873f3c8cb2e8870174e208c8db1632bf0f7d.

--- device.mk
+++ device.mk
@@ -97,13 +97,20 @@ PRODUCT_COPY_FILES += \
      
 # Bluetooth
 PRODUCT_PACKAGES += \
-   libbluetooth_qti
+    android.hardware.bluetooth.audio@2.0-impl \
+    audio.bluetooth.default \
+    BluetoothQti \
+    libbthost_if \
+    libldacBT_dec \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor
     
     PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
 
-
+PRODUCT_COPY_FILES += \
+    hardware/qcom-caf/sm8150/audio/configs/common/bluetooth_qti_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_qti_audio_policy_configuration.xml
 
 # Camera
 PRODUCT_PACKAGES += \
